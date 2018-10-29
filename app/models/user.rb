e603e29a48c4c91acd6fd42c0d4eb1e145d87b70
olistik <root@olisti.co>
2018-10-29 18:46:11 +0100

    Implements the admin scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   include Clearance::User
   enum app_role: [ :administrator, :provider, :shop, :warehouse_worker ]
   has_one :provider
+
+  scope :admin, -> { where(app_role: :administrator) }
 end
