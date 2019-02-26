f34eb49558f1b0b00f1067b174de892cf0cced1a
olistik <root@olisti.co>
2019-02-26 22:46:38 +0100

    Subtracts point rank instead of shopping total

--- app/models/update_beneficiary_points_after_shopping.rb
+++ app/models/update_beneficiary_points_after_shopping.rb
@@ -1,10 +1,11 @@
 module UpdateBeneficiaryPointsAfterShopping
   def self.call(shopping:)
     beneficiary = shopping.beneficiary
-    current = beneficiary.shopping_points
-    return Result.error.code!(:not_enough_points) if current < shopping.total
-
-    result = beneficiary.update(shopping_points: current - shopping.total)
+    if beneficiary.shopping_points < beneficiary.point_rank
+      return Result.error.code!(:not_enough_points)
+    end
+    value = beneficiary.shopping_points - beneficiary.point_rank
+    result = beneficiary.update(shopping_points: value)
     return Result.error.code!(:cant_save_db_record) unless result
 
     Result.success
