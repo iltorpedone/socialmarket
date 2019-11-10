9fbf5b7225eefd7cf6038dfef38cb5ae42dbc6d4
olistik <root@olisti.co>
2019-11-10 10:36:45 +0100

    Updates the failing specs.

--- spec/factories/beneficiaries.rb
+++ spec/factories/beneficiaries.rb
@@ -3,5 +3,11 @@ FactoryBot.define do
     shopping_points { 50 }
     provider
     city
+    family_components_count_2_5 { 0 }
+    family_components_count_6_12 { 0 }
+    family_components_count_13_18 { 0 }
+    family_components_count_19_30 { 0 }
+    family_components_count_30_65 { 0 }
+    family_components_count_over_65 { 0 }
   end
 end
--- spec/models/update_beneficiary_points_after_shopping_spec.rb
+++ spec/models/update_beneficiary_points_after_shopping_spec.rb
@@ -2,23 +2,23 @@ require 'rails_helper'
 
 RSpec.describe UpdateBeneficiaryPointsAfterShopping do
   describe '.call' do
-    context 'with a beneficiary with 50 points' do
-      let!(:beneficiary) { create(:beneficiary, shopping_points: 50) }
+    context 'with a beneficiary with 150 points and 80 point rank' do
+      let!(:beneficiary) do
+        create(
+          :beneficiary,
+          shopping_points: 150,
+          family_components_count_0_1: 1, # point rank: 80
+        )
+      end
 
-      context 'with a shopping with a total of 10 points' do
-        let!(:shopping) do
-          create(:shopping, total: 10, beneficiary: beneficiary)
-        end
+      subject do
+        UpdateBeneficiaryPointsAfterShopping.call(beneficiary: beneficiary)
+      end
 
-        subject do
-          UpdateBeneficiaryPointsAfterShopping.call(shopping: shopping)
-        end
-
-        it 'updates the beneficiary shopping points to 40' do
-          expect(subject).to be_success
-          beneficiary.reload
-          expect(beneficiary.shopping_points).to eq(40)
-        end
+      it 'updates the beneficiary shopping points to 70' do
+        expect(subject).to be_success
+        beneficiary.reload
+        expect(beneficiary.shopping_points).to eq(70)
       end
     end
   end
