8cb3cdda7dca6de6732df159e056355427c69b50
Maurizio De Magnis <root@olisti.co>
2021-10-22 22:48:22 +0200

    Sets the new point rank threshold
    The purchase window is now sized to 3 points.

--- spec/models/validate_shopping_total_spec.rb
+++ spec/models/validate_shopping_total_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe ValidateShoppingTotal do
   describe '.call' do
-    # This is an arbitrary value, should be anything > 10.
+    # This is an arbitrary value, should be anything >= 0.
     let(:point_rank) { 13 }
 
-    it 'validates all values within (p-10..p)' do
-      ((point_rank - 10)..(point_rank)).each do |total|
+    it 'validates all values within (p..p+3)' do
+      ((point_rank)..(point_rank + 3)).each do |total|
         result = ValidateShoppingTotal.call(
           total: total,
           point_rank: point_rank,
@@ -16,15 +16,7 @@ RSpec.describe ValidateShoppingTotal do
     end
 
     it 'does not validate values that are outside (p-10..p)' do
-      ((point_rank - 20)..(point_rank - 11)).each do |total|
-        result = ValidateShoppingTotal.call(
-          total: total,
-          point_rank: point_rank,
-        )
-        expect(result).to be_error
-      end
-
-      ((point_rank + 1)..(point_rank + 10)).each do |total|
+      [point_rank - 1, point_rank + 4].each do |total|
         result = ValidateShoppingTotal.call(
           total: total,
           point_rank: point_rank,
